55de57329e9155969d1f368f97764f30d2ac63e0
wangxueyang <wangxueyang@wecash.net>
2019-11-22 10:14:58 +0800

    lintcode 119 unfinish

---
+++ lintcode/119_edit_distance.py
@@ -0,0 +1,53 @@
+"""
+#动态规划#
+119. Edit Distance
+
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+
+Example 1:
+
+Input:
+"horse"
+"ros"
+Output: 3
+Explanation:
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+
+
+Example 2:
+
+Input:
+"intention"
+"execution"
+Output: 5
+Explanation:
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+"""
+
+
+class MySolution:
+    """
+    @param word1: A string
+    @param word2: A string
+    @return: The minimum number of steps.
+    """
+
+    def minDistance(self, word1, word2):
+        # write your code here
+        return
+
+
+if __name__ == "__main__":
+    assert MySolution().minDistance("horse", "ros") == 3
--- lintcode/667_longest_palindromic_subsequence.py
+++ lintcode/667_longest_palindromic_subsequence.py
@@ -3,6 +3,9 @@
 
 Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000
 
+
+#动态规划#
+
 """
 
 __author__ = 'qingxi'
